27810a39e40306f768656b3db2630b67b43b1831
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2026-01-12 09:58:33 +0800

    new

---
+++ HifyLive/HifyLiveApp.swift
@@ -0,0 +1,18 @@
+//
+//  HifyLiveApp.swift
+//  HifyLive
+//
+//  Created by yangyang on 2026/1/12.
+//
+
+import SwiftUI
+
+@main
+struct HifyLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //启动view
+           // ContentView()
+        }
+    }
+}
